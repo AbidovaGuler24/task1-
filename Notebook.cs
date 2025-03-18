3a17684b685ca3eacbd02c95c0ec193e77039c9b
AbidovaGuler <gullarabidova@gmail.com>
2025-03-18 20:48:14 +0400

    task

---
+++ Notebook.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Notebook : Product
+    {
+        private byte Ram;
+        public int Storage;
+        public byte RAM
+        {
+            get
+            { 
+                return Ram;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    Ram = value;
+                }
+
+                else
+                {
+                    
+                    Console.WriteLine("RAM sıfırdan büyük olmalıdır");
+                }
+            }
+        }
+        public Notebook (string name, string description, int count, double price, byte ram, int storage): base(name, description, count, price)
+        {
+            this.Ram=ram;
+            this.Storage=storage;
+
+
+        }
+        public string GetFullInfo()
+        {
+            return $"Notebook: {Name}, Açıklama: {Description}, Stok: {Count}, Fiyat: {Price}$, RAM: {RAM}GB, Depolama: {Storage}GB";
+        }
+        public void ShowFullData()
+        {
+            Console.WriteLine(GetFullInfo());
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           Notebook notebook = new Notebook("Asus ROG", "Gaming Laptop", 0, 2500.99, 16, 512);
+            notebook.Sale();
+            notebook.ShowFullData();
+
+        }
+    }
+    public class Product
+    {
+       public string Name;
+       public string Description;
+       public int Count;
+       public bool IsStock;
+       private double price;
+       public double Price
+        {
+            get
+            {
+                return price;
+
+            }
+            set
+            {
+                if (value >= 0)
+                    price = value;
+
+            }
+         }
+        public Product(string name, string description, int count, double price)
+        {
+           
+
+            Name = name;
+            Description = description;
+            Count = count;
+            Price = price;
+        }
+        public  void Sale()
+        {
+            if (Count>0)
+            {
+                
+                Console.WriteLine(" satıldı.");
+            }
+            else
+            {
+                Console.WriteLine("Məhsul yoxdur!");
+            }
+        }
+    } 
+}
